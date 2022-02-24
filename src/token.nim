0d4eb0186b0087d4f947e83c0736e43c2b455206
minhtriet2k9 <minhtrietamoled@gmail.com>
2022-02-24 09:24:10 +0700

    update many things

--- src/token.nim
+++ src/token.nim
@@ -13,12 +13,15 @@ type
   # quit(1)
 
 
-iterator myTokenize(str: string): tuple[identifier: string, isInt: bool] =
+iterator myTokenize(str: string, line: int): tuple[identifier: string,
+    isInt: bool, index: int] =
   var isId = false
   var isInt = false
   var isStr = false
   var retId = ""
   var retIsInt = false
+  var line = 1
+  var index = 1
 
   proc reset() =
     retId = ""
@@ -29,24 +32,44 @@ iterator myTokenize(str: string): tuple[identifier: string, isInt: bool] =
 
   for i in 0..str.len()-1:
     let endLine = i == str.len()-1
+    index = i+1
+
+    if isStr: 
+      retId &= str[i]
+
     if str[i] in Letters:
       isId = true
       retId &= str[i]
+      index += 1
+    elif str[i] in Digits:
+      isInt = true
+      retId &= str[i]
+      index += 1
+    elif str[i] == '"':
+      if isStr:
+        isStr = false
+      else:
+        isStr = true
+
     if (str[i] notin Letters or endLine) and isId:
       isId = false
-      yield (identifier: retId, isInt: retIsInt)
+      index = index - retId.len()
+      yield (identifier: retId, isInt: retIsInt, index: index)
+      reset()
+    elif (str[i] notin Digits or endLine) and isInt:
+      isInt = false
+      retIsInt = true
+      index = index - retId.len()
+      yield (identifier: retId, isInt: retIsInt, index: index)
       reset()
 
 iterator tokenize*(lines: var seq[string]): Token =
   lines.add("")
   var tok: Token
   for i in 0..<lines.len()-1:
-    var index = 1
-    for (tokid, isInt) in lines[i].myTokenize():
+    for (tokid, isInt, index) in lines[i].myTokenize(i+1):
       tok = Token(identifier: tokid.strip(leading = false).strip(
-          trailing = false), line: i+1, index: index, isInt: isInt)
-      echo tokid
-      index += tokid.len()
+          trailing = false), index: index, isInt: isInt, line: i+1)
       yield tok
 
 
