7285973182910999398abea45ac38f16834b3699
minhtriet2k9 <minhtrietamoled@gmail.com>
2022-02-24 11:01:39 +0700

    update many things

--- docs/Error.md
+++ docs/Error.md
@@ -6,3 +6,8 @@ This happened when a string is undelimited.<br />
 You maybe forgor the `"` (double quotes) at the end of the string<br />
 Example:<br />
 `" this is an undelimited string with missing double quotes`
+
+## Error #2
+
+This happend when a `\` missing a chracter after it.<br />
+Use of `\` is similar to any programming language
--- src/token.nim
+++ src/token.nim
@@ -22,31 +22,46 @@ iterator myTokenize(str: string, line: int): tuple[identifier: string,
   var st = ""
   var retIsInt = false
   var index = 0
+  var isChar = false
+  var i = -1
 
   proc reset() =
     retId = ""
     st = ""
+    isChar = false
     retIsInt = false
     isInt = false
     isStr = false
     isId = false
 
-  for i in 0..str.len()-1:
+  while i < str.len()-1:
+    i += 1
     let endLine = i == str.len()-1
     index = i+1
 
     if isStr:
       st &= str[i]
 
-    if str[i] in Letters and not isStr:
+    if isChar:
+      isChar = false
+
+    if str[i] == '\\' and isStr:
+      isChar = true
+      if i == str.len()-1:
+        echo "Error #2 at (", line, ", ", index, "): Missing character before `\\`"
+        quit(1)
+      st &= str[i+1]
+      i += 1
+
+    if str[i] in Letters and not isStr and not isChar:
       isId = true
       retId &= str[i]
       index += 1
-    elif str[i] in Digits and not isStr:
+    elif str[i] in Digits and not isStr and not isChar:
       isInt = true
       retId &= str[i]
       index += 1
-    elif str[i] == '"':
+    elif str[i] == '"' and not isChar:
       if isStr:
         isStr = false
       else:
@@ -62,8 +77,16 @@ iterator myTokenize(str: string, line: int): tuple[identifier: string,
       reset()
     elif not isStr and st != "":
       retId = st[0..^2]
+      retId = retId.replace("\\n", "\n")
+      retId = retId.replace("\\t", "\t")
+      retId = retId.replace("\\r", "\r")
+      retId = retId.replace("\\", "")
       yield (identifier: retId, isInt: retIsInt, index: index)
       reset()
+
+  if isStr:
+    echo "Error #1 at (", line, ",", index, "): Undelimited string. Maybe you are missing `\"`?"
+    quit(1)
 
 iterator tokenize*(lines: var seq[string]): Token =
   lines.add("")
